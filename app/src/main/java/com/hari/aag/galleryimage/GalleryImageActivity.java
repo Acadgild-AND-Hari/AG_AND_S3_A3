4bee2cc24602d8b3a496a8c70120abdd4d6239b9
harinivaskumar <harinivaskumar@live.com>
2016-12-13 03:11:13 +0530

    Code Commit
    Initial Code

---
+++ app/src/main/java/com/hari/aag/galleryimage/GalleryImageActivity.java
@@ -0,0 +1,13 @@
+package com.hari.aag.galleryimage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GalleryImageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gallery_image);
+    }
+}
